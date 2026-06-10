24165315fd339df5beb4c676aa19356cb45f58f4
Angelo Veltens <angelo.veltens@codecentric.de>
2026-06-10 06:48:45 +0200

    keep original dirname as workdir

--- pi-run.sh
+++ pi-run.sh
@@ -7,14 +7,16 @@ echo "Pulling latest image: ${IMAGE}"
 docker pull "${IMAGE}"
 
 echo "Starting pi-coding-agent container..."
+DIR_NAME=$(basename "$(pwd)")
+
 docker run -it \
   --cap-drop=ALL \
   --network=host \
-  -v $(pwd):/workspace:rw \
+  -v $(pwd):/workdir/${DIR_NAME}:rw \
   -v ~/.pi:/home/node/.pi:rw \
   -v ~/.wallaby:/home/node/.wallaby:ro \
   -v ~/.agents:/home/node/.agents:ro \
   -e GNUPGHOME=/home/node/.pi/gnupg \
   -e PASSWORD_STORE_DIR=/home/node/.pi/secrets \
-  -w /workspace \
+  -w /workdir/${DIR_NAME} \
   "${IMAGE}"
